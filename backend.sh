30dc3f57022a56369c8680b4054bcbdd338cc3ac
avsuryateja991 <avsuryateja991@gmail.com>
2025-07-11 17:05:23 +0530

    automating expense project

--- backend.sh
+++ backend.sh
@@ -47,7 +47,7 @@ useradd expense &>>$LOG_FILE_NAME
 #VALIDATE $? "user already exist"
 
 mkdir /app &>>$LOG_FILE_NAME
-VALIDATE $? "folder exist"
+#VALIDATE $? "folder exist"
 #VALIDATE $? "app foldr created"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
